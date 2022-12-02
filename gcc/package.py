fed1db68a36858d829e7d4c645e2dbd26c3df6b1
Ben <bhawkyard1@gmail.com>
2022-12-02 17:02:34 +0000

    cd to dl prereqs.

--- gcc/package.py
+++ gcc/package.py
@@ -25,14 +25,14 @@ def pre_build_commands():
         f"gcc-{this.version}.tar.gz"
     )
     env.UNTAR = f"tar xzvf gcc-{this.version}.tar.gz"
-    env.PREREQS = f"gcc-{this.version}/contrib/download_prerequisites"
+    env.PREREQS = f"cd gcc-{this.version} && ./contrib/download_prerequisites"
 
     path = build.build_path
     if build.install:
         path = build.install_path
 
     env.CONFIGURE = (
-        f"./gcc-{this.version}/configure --prefix={path} "
+        f"./configure --prefix={path} "
         f"--enable-languages=c,c++"
     )
     env.MAKE = "make -j 4"
